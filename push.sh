df74425822a954cd36a7988ce695e6e6afa48363
Luigi Arena <arenaluigi14@gmail.com>
2025-04-15 13:10:48 +0200

    Empty

--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-
-git add .
-git commit -m "$TIMESTAMP"
-git branch -M main
-git remote add origin git@github.com:larena14/NS-Attack1Scenario.git
-git push -u origin main
